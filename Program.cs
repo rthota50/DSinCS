5b24744f0c5d0cf126fbc61112835cbfd3a7fe2b
Rajiv Thota <rajivthota@rthota.local>
2015-10-25 17:56:38 -0700

    creating ds in cs repo

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace DSinCS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
