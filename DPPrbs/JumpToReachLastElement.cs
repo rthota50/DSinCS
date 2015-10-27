d844a04af7522c5bc240c117c22ee3077ce2f87b
Rajiv Thota <rajivthota@rthota.local>
2015-10-27 09:04:28 -0700

    added path to reach end of array in min jumps

--- DPPrbs/JumpToReachLastElement.cs
+++ DPPrbs/JumpToReachLastElement.cs
@@ -10,6 +10,7 @@ public class ReachLastElement
 		this._jumps = int.MaxValue;
 	}
 	private int _jumps;
+	
 	#region brute force
 	public int OptimalJumps(int[] a)
 	{
@@ -49,21 +50,31 @@ public class ReachLastElement
 	public int OptimalJumps_DP1(int[] a)
 	{
 		int n = a.Length;
-		var Res = new int[n];
-		Res[0] = 0;
-		for(int i=1; i<n; i++) //2
+		var jumps = new int[n];
+		var path = new int[n];
+		jumps[0] = 0;
+		path[0] = 0;
+		for(int i=1; i<n; i++)
 		{
-			Res[i] = int.MaxValue; //[2]-> max
+			jumps[i] = int.MaxValue-1; // -1 to avoid over flow 
 			for(int j=0; j<i; j++)
 			{
-				if(i <= j+a[j] && Res[j] != int.MaxValue)
+				if(i <= j+a[j])
 				{
-					Res[i] = Math.Min(Res[i], Res[j]+1); // int_max vs 1
-					break;	
+					if(jumps[i] > jumps[j]+1)
+					{
+						jumps[i] = jumps[j]+1;
+						path[i] = j;
+						break;	
+					}
 				}						
 			}
 		}
-		return Res[n-1];
+		for(int i=0; i<path.Length; i++)
+			Console.Write(path[i]+" ");
+		Console.WriteLine();
+		
+		return jumps[n-1];
 	}
 	
 	/*
@@ -100,15 +111,17 @@ public class ReachLastElement
 		return jumps[0];
 	}
 	
-	
 	#endregion
+	
 	public static void Test()
 	{
 		var a = new int[]{2,3,1,1,4};
+		var b = new int[]{1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
+		var c = new int[]{1, 3, 6, 1, 0, 9};
+		var d = new int[]{2,3,1,1,2,4,2,0,1,1};
 		var prbs = new ReachLastElement();
 		var res = prbs.OptimalJumps(a);
 		Console.WriteLine(res);		
-		var b = new int[]{1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9};
 		var res2 = prbs.OptimalJumps(b);
 		Console.WriteLine(res2);
 		
@@ -116,9 +129,9 @@ public class ReachLastElement
 		Console.WriteLine(res_dp1);
 		var res_dp1_2 = prbs.OptimalJumps_DP1(b);
 		Console.WriteLine(res_dp1_2);
-		var c = new int[]{1, 3, 6, 1, 0, 9};
 		var res_dp1_3 = prbs.OptimalJumps(c);
 		Console.WriteLine(res_dp1_3);
+		Console.WriteLine("DP1 : {0}",prbs.OptimalJumps_DP1(d));
 		
 		var res_dp2 = prbs.MinJumps_DP2(a);
 		Console.WriteLine(res_dp2);
