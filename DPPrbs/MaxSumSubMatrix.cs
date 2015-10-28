df743437c5283d5a75fe05e84d62bddc3f9d5d4b
Rajiv Thota <rajivthota@rthota.local>
2015-10-27 22:21:37 -0700

    added kadanes logic

--- DPPrbs/MaxSumSubMatrix.cs
+++ DPPrbs/MaxSumSubMatrix.cs
@@ -5,6 +5,10 @@ public class MaxSumSubMatrix
 	Give a m*n matrix of positive and negative numbers, find the submatrix with maximum sum
 	*/
 	#region BruteForce
+	/*
+	Time Complexity: O(n*n*n*n)
+	Space Complexiry: O(1)
+	*/
 	public int MaxSum(int[,] m)
 	{
 		int sum = int.MinValue;
@@ -34,6 +38,42 @@ public class MaxSumSubMatrix
 		return sum;		
 	}
 	#endregion
+	
+	#region Dynamic programming
+	public int MaxSum_DP(int[,] m)
+	{
+		int sum = int.MinValue;
+		
+		return sum;
+	}
+	//Kadane's algo to find max sub-array
+	public void MaxContiguousSubArray(int[] t, out int sum, out int start, out int end)
+	{
+		sum = 0;
+		start = end = 0;
+		int currSum, maxSum;
+		maxSum = currSum = t[0];
+		for(int i=1; i<t.Length; i++)
+		{
+			if(t[i] > currSum+t[i])
+			{
+				currSum = t[i];
+				end = start = i;
+			}
+			else
+			{
+				currSum += t[i];
+			}
+			if(maxSum < currSum)
+			{
+				maxSum = currSum;
+				end = i;
+			}
+		}
+		sum = maxSum;
+	}
+	#endregion
+	
 	public static void Test()
 	{
 		var m = new int[,]{	{ 1, 2,-1,-4,-20},
@@ -42,5 +82,10 @@ public class MaxSumSubMatrix
 							{-4,-1, 1, 7, -6}};	
 		var prb = new MaxSumSubMatrix();
 		Console.WriteLine(prb.MaxSum(m));
+		
+		var t = new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+		int sum, st, ed;
+		prb.MaxContiguousSubArray(t, out sum, out st, out ed);
+		Console.WriteLine("Sum "+sum+" st "+st+" ed "+ed);
 	}
 }
